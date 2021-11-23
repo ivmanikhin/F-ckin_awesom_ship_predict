7c8ebaef403f33681db63b816e54f6e86b6ea793
ivmanikhin <ivmanikhin@gmail.com>
2021-11-23 17:03:13 +0300

    now it can read

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ TOP_LEFT = [230, 114, 227, 114]
 FIELD_COORDS = {"reg_num": [0, 0, 9, 46],
                 "imo_num": [0, 46, 9, 90]
                 }
-reg_num = tabula.read_pdf("000.pdf", area=np.add(FIELD_COORDS["reg_num"], TOP_LEFT).tolist(), pages="1")
-imo_num = tabula.read_pdf("000.pdf", area=np.add(FIELD_COORDS["imo_num"], TOP_LEFT).tolist(), pages="1")
+reg_num = tabula.read_pdf("000.pdf", area=np.add(FIELD_COORDS["reg_num"], TOP_LEFT).tolist(), pages="1", pandas_options={"header": None})
+imo_num = tabula.read_pdf("000.pdf", area=np.add(FIELD_COORDS["imo_num"], TOP_LEFT).tolist(), pages="1", pandas_options={"header": None})
 print(np.add(FIELD_COORDS["reg_num"], TOP_LEFT).tolist())
-print(reg_num[0], "\n", imo_num[0])
+print(reg_num[0][0][0])
+print(imo_num[0][0][0])
 
 # file = open('000.pdf', "rb")
 # input_pdf = ppdf.PdfFileReader(file)
